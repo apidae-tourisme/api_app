0a32fc698e50ad8e537dc017b9d07dec90da92dc
Jean-Baptiste Vilain <jbvilain@gmail.com>
2016-10-25 10:41:25 +0100

    Fix unsafe url issues

--- src/pages/details/details.ts
+++ src/pages/details/details.ts
@@ -1,6 +1,7 @@
 import {Component} from '@angular/core';
 import {NavParams, Events, ViewController} from 'ionic-angular';
 import {Seed} from "../../components/seed.model";
+import {DomSanitizer} from "@angular/platform-browser";
 
 @Component({
   templateUrl: 'details.html'
@@ -10,12 +11,16 @@ export class DetailsPage {
   public node: Seed;
 
   constructor(public viewCtrl: ViewController, params: NavParams,
-              public events: Events) {
+              public events: Events, private sanitizer:DomSanitizer) {
     this.node = new Seed(params.get('node'), false, false);
   }
 
   closeModal(): void {
     this.viewCtrl.dismiss();
     // this.events.publish('actions:hide');
+  }
+
+  sanitizeUrl(url) {
+    return this.sanitizer.bypassSecurityTrustUrl(url);
   }
 }
