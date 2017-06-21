17addf2f02f4000e9fbae5f06c7673464330d466
Jean-Baptiste Vilain <jbvilain@gmail.com>
2017-06-21 09:00:03 +0200

    Partly implemented PouchDB-CouchDB use

--- src/providers/apiapp.config.ts
+++ src/providers/apiapp.config.ts
@@ -1,11 +1,17 @@
 export class ApiAppConfig {
   public static readonly API_URL = '';
+
+  // OAuth & SSO config
   public static readonly OAUTH_REDIRECT_URL = '';
   public static readonly OAUTH_AUTH_URL = '';
   public static readonly OAUTH_TOKEN_URL = '';
   public static readonly OAUTH_PROFILE_URL = '';
   public static readonly OAUTH_CLIENT_ID = '';
   public static readonly OAUTH_SECRET = '';
+
+  // Local and remote databases config
+  public static readonly DB_NAME = '';
+  public static readonly DB_URL = '';
 
   public static authUrl(redirectUrl): string {
     return ApiAppConfig.OAUTH_AUTH_URL + '?client_id=' + ApiAppConfig.OAUTH_CLIENT_ID + '&redirect_uri=' +
