abe4ad81278269f90544f822d439db5b24b7cb74
Jean-Baptiste Vilain <jbvilain@gmail.com>
2017-10-04 22:32:30 +0200

    Web version with working worker.

---
+++ patched/utils.js
@@ -0,0 +1,100 @@
+'use strict';
+
+// Patched : in node_modules/worker-pouch/lib/shared/utils.js (Webpack compatibility issue)
+var Promise = require('pouchdb-promise').default;
+
+exports.lastIndexOf = function lastIndexOf(str, char) {
+  for (var i = str.length - 1; i >= 0; i--) {
+    if (str.charAt(i) === char) {
+      return i;
+    }
+  }
+  return -1;
+};
+
+exports.clone = require('./pouchdb-clone');
+
+/* istanbul ignore next */
+exports.once = function once(fun) {
+  var called = false;
+  return exports.getArguments(function (args) {
+    if (called) {
+      if ('console' in global && 'trace' in console) {
+        console.trace();
+      }
+      throw new Error('once called  more than once');
+    } else {
+      called = true;
+      fun.apply(this, args);
+    }
+  });
+};
+/* istanbul ignore next */
+exports.getArguments = function getArguments(fun) {
+  return function () {
+    var len = arguments.length;
+    var args = new Array(len);
+    var i = -1;
+    while (++i < len) {
+      args[i] = arguments[i];
+    }
+    return fun.call(this, args);
+  };
+};
+/* istanbul ignore next */
+exports.toPromise = function toPromise(func) {
+  //create the function we will be returning
+  return exports.getArguments(function (args) {
+    var self = this;
+    var tempCB = (typeof args[args.length - 1] === 'function') ? args.pop() : false;
+    // if the last argument is a function, assume its a callback
+    var usedCB;
+    if (tempCB) {
+      // if it was a callback, create a new callback which calls it,
+      // but do so async so we don't trap any errors
+      usedCB = function (err, resp) {
+        process.nextTick(function () {
+          tempCB(err, resp);
+        });
+      };
+    }
+    var promise = new Promise(function (fulfill, reject) {
+      try {
+        var callback = exports.once(function (err, mesg) {
+          if (err) {
+            reject(err);
+          } else {
+            fulfill(mesg);
+          }
+        });
+        // create a callback for this invocation
+        // apply the function in the orig context
+        args.push(callback);
+        func.apply(self, args);
+      } catch (e) {
+        reject(e);
+      }
+    });
+    // if there is a callback, call it back
+    if (usedCB) {
+      promise.then(function (result) {
+        usedCB(null, result);
+      }, usedCB);
+    }
+    promise.cancel = function () {
+      return this;
+    };
+    return promise;
+  });
+};
+
+exports.inherits = require('inherits');
+exports.Promise = Promise;
+
+var binUtil = require('pouchdb-binary-util');
+
+exports.createBlob = binUtil.createBlob;
+exports.readAsArrayBuffer = binUtil.readAsArrayBuffer;
+exports.readAsBinaryString = binUtil.readAsBinaryString;
+exports.binaryStringToArrayBuffer = binUtil.binaryStringToArrayBuffer;
+exports.arrayBufferToBinaryString = binUtil.arrayBufferToBinaryString;
